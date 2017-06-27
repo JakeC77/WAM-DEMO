9713c1e13d8c8a28e7912f034f19a57e9d8a18d7
JakeC77 <jake0christensen@gmail.com>
2017-06-27 14:57:41 -0700

    Initial Commit: basic search is working

---
+++ WAM-DEMO/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WAM_DEMO.Models;
+
+namespace WAM_DEMO.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Search(string query)
+        {
+            var data = Record.GetDummyData();
+            var searched = data.Where(d => d.FirstName.Contains(query) || d.LastName.Contains(query) || d.Position.Contains(query));
+            return Json(searched);
+        }
+    }
+}
---
+++ WAM-DEMO/Models/Record.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAM_DEMO.Models
+{
+    public class Record
+    {
+        public string Position { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static List<Record> GetDummyData()
+        {
+            var records = new List<Record>();
+            records.Add(new Record
+            {
+                FirstName = "Bob",
+                LastName = "Loblaw",
+                Position = "Lawyer/Blogger"
+            });
+            return records;
+            
+        }
+
+    }
+}
